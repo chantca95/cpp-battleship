41c146e76b0d18474135066eeb36ac93a7a06e6c
Alex Chan <chantca95@meta.com>
2023-02-14 23:47:30 -0800

    Initial Commit

---
+++ cpp-battleship/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp-battleship
+//
+//  Created by Alex Chan on 2/14/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
